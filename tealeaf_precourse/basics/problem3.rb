3ef1c85b807389c9f9c2b4e5519cdcc3cbc33d5d
Philip Ainza <philainza@gmail.com>
2014-06-08 18:10:37 -0700

    Simplify output commands

--- tealeaf_precourse/basics/problem3.rb
+++ tealeaf_precourse/basics/problem3.rb
@@ -4,8 +4,4 @@ movies = { Tron: 1985,
            Transformers: 2007,
            Transformers_Dark_of_the_Moon: 2011 }
 
-puts movies[:Tron]
-puts movies[:Tron_Legacy]
-puts movies[:I_Robot]
-puts movies[:Transformers]
-puts movies[:Transformers_Dark_of_the_Moon]
+puts movies.values

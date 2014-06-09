808bccb1b74df3b1b094af8013bc2850d8170449
Philip Ainza <philainza@gmail.com>
2014-06-08 19:59:29 -0700

    Store names in variables and concatenate

--- tealeaf_precourse/02_variables/name.rb
+++ tealeaf_precourse/02_variables/name.rb
@@ -1,6 +1,8 @@
-puts "Please enter your name"
-name = gets.chomp
-puts "Hi, " + name
+puts "Please enter your first name"
+first_name = gets.chomp
+puts "Please enter your last name"
+last_name = gets.chomp
+puts "Hi, " + first_name + " " + last_name
 
 # Problem 3
 

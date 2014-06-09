243b6e80d8d90b7c65fa7ea6a5a993c7bf0d96bb
Philip Ainza <philainza@gmail.com>
2014-06-08 18:12:00 -0700

    change hash to array

---
+++ tealeaf_precourse/basics/problem4.rb
@@ -0,0 +1,10 @@
+movies = { Tron: 1985,
+           Tron_Legacy: 2010,
+           I_Robot: 2004,
+           Transformers: 2007,
+           Transformers_Dark_of_the_Moon: 2011 }
+ 
+years = movies.values.to_a
+
+puts years
+

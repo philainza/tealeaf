26f64358b8faf871720c47fe5a2aa2cdfb3d16c2
Philip Ainza <philainza@gmail.com>
2014-06-08 17:33:33 -0700

    make a hash to store movies

---
+++ tealeaf_precourse/basics/problem3.rb
@@ -0,0 +1,11 @@
+movies = { Tron: 1985,
+           Tron_Legacy: 2010,
+           I_Robot: 2004,
+           Transformers: 2007,
+           Transformers_Dark_of_the_Moon: 2011 }
+
+puts movies[:Tron]
+puts movies[:Tron_Legacy]
+puts movies[:I_Robot]
+puts movies[:Transformers]
+puts movies[:Transformers_Dark_of_the_Moon]

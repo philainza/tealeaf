e41548c47b7bfdd6cc6bae741280694d9f586c0d
Philip Ainza <philainza@gmail.com>
2014-06-09 13:24:26 -0700

    fix use each_with_index

--- tealeaf_precourse/05_loops_iterators/exercise3.rb
+++ tealeaf_precourse/05_loops_iterators/exercise3.rb
@@ -1,7 +1,4 @@
-california_cities = ["San Diego", 
-					"Los Angeles", 
-					"San Francisco", 
-					"Santa Barbara"]
+california_cities = ["San Diego", "Los Angeles", "San Francisco", "Santa Barbara"]
 
 california_cities.each_with_index do | city, index |
 	puts "#{index + 1}: #{city}"

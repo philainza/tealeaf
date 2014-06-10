d0593a4b0441d6c57d2d4039a6affa74ea6cd091
Philip Ainza <philainza@gmail.com>
2014-06-09 18:22:31 -0700

    do random stuff to hash

--- tealeaf_precourse/09_exercises/problem9.rb
+++ tealeaf_precourse/09_exercises/problem9.rb
@@ -1,3 +1,17 @@
-h = { a:1, b:2, c:3, d:4}
+# part 1
 
+h = { a:1, b:2, c:3, d:4}
 puts h.fetch(:b) 
+
+# part 2
+
+h[:e] = 5
+puts h
+
+h.each do |key, value|
+	if value < 3.5
+		h.delete(key)
+	end
+end
+
+puts h

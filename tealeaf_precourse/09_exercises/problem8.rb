1d5fac25c4088448a80fad64b87124f5be40f7a1
Philip Ainza <philainza@gmail.com>
2014-06-09 18:06:19 -0700

    use both hash syntax styles

---
+++ tealeaf_precourse/09_exercises/problem8.rb
@@ -0,0 +1,4 @@
+hash_1 = { california: 'San Diego', arizona: 'Tucson', washington: 'Seattle', oregon: 'Portland'}
+
+hash_2 = { :california => 'San Diego', :arizona => 'Tucson', :washington => 'Seattle', :oregon => 'Portland'}
+
